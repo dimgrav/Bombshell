fd5a6e2a1569139a61f5f026d58f471f539e509b
Dimitris Gravanis <dim_grav@yahoo.gr>
2017-08-21 11:28:28 +0300

    Update dispindex.sh

--- dispindex.sh
+++ dispindex.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
-
 # dispindex: A simple script to display directory contents
+#
 # Copyright 2017 (c) Dimitrios Gravanis
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
+# the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -18,6 +17,8 @@
 
 # input:	user-specified path
 # output:	path contents
+
+#!/bin/bash
 
 function index() {
 	read -p "directory path (q to quit): " FILEPATH
