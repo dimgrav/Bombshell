67f621ee759584256d42a7be8581bfa447215905
Dimitris Gravanis <dim_grav@yahoo.gr>
2017-09-22 21:54:14 +0300

    Update copier.sh

--- copier.sh
+++ copier.sh
@@ -16,7 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # input:	user-specified path
-# output:	user-specified path
+# output:	source path contents
 
 #!/bin/bash
 
